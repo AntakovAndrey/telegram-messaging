f1291a89a3f5f07f477b662826c5f9c4aead3e12
AntakovAndrey <andrey.antakov05@gmail.com>
2024-07-09 21:33:20 +0300

    client class

---
+++ messaging-service/Client.cs
@@ -0,0 +1,7 @@
+class Client{
+    public int Id {get;set;}
+    public int ApiId{get;set;}
+    public string ApiHash{get;set;}
+    public string Phone{get;set;}
+    public string? SecureCode{get;set}
+}
