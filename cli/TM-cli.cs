c57f8ec9406ec7c64f602d3b7a7d14a15e58368d
AntakovAndrey <andrey.antakov05@gmail.com>
2024-07-16 14:44:17 +0300

    selecting subcommand logics added

--- cli/TM-cli.cs
+++ cli/TM-cli.cs
@@ -16,20 +16,31 @@ class Subcommand{
 
 class Program{
     private static Subcommand[] subcommands={
-        new Subcommand("show",""),
-        new Subcommand("",""),
+        new Subcommand("show","Shows client configuration."),
+        new Subcommand("showchats","Shows client's chats."),
+        new Subcommand("showtasks","Shows all tasks applied to client."),
+        new Subcommand("genconf","Create config."),
+        new Subcommand("setconf","Applies a configuration file."),
+        new Subcommand("set","Change the current configuration.")
     };
     public static void ShowHelp()
     {
         Console.Write("Usage: <cmd> [<args>]\n\n");
+        Console.Write("Available subcommands:\n");
         foreach(var subcommand in subcommands)
         {
-            Console.WriteLine($"{subcommand.SubcommandName} {subcommand.Description}");
+            Console.WriteLine($"{subcommand.SubcommandName}: {subcommand.Description}");
         }
-        Console.WriteLine("You may pass --help to any of this subcommands to view usage.");
+        Console.WriteLine("\nYou may pass --help to any of this subcommands to view usage.");
     }
     public static int Main(string[] args)
     {
+        Console.ForegroundColor=ConsoleColor.Gray;
+        if(args.Length==0)
+        {
+            ShowHelp();
+            return 1;
+        }
         if(args.Length==1&&(args[0]=="-v"||args[0]=="version"||args[0]=="--version"))
         {
             Console.WriteLine("Telegram-messaging CLI\nv 1.0.0");
@@ -40,11 +51,17 @@ class Program{
             ShowHelp();
             return 0;
         }
-        
-        var defaultColor = Console.ForegroundColor;
+        foreach(var subcommand in subcommands)
+        {
+            if(subcommand.SubcommandName==args[0])
+            {
+                Console.WriteLine(subcommand.Description);
+                return 0;
+            }
+        }
         Console.ForegroundColor=ConsoleColor.Red;
-        Console.WriteLine("Invalid subcommand "+args[0]);
-        Console.ForegroundColor = defaultColor;
+        Console.WriteLine("Invalid subcommand "+args[0]+"\n");
+        Console.ForegroundColor = ConsoleColor.Gray;
         ShowHelp();
 
         return 1;
