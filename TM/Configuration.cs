cf6a1b72ddb6f1a16432749d98981dc4ec0327b1
AntakovAndrey <andrey.antakov05@gmail.com>
2024-08-04 19:42:58 +0300

    configuration odd methods removed

--- TM/Configuration.cs
+++ TM/Configuration.cs
@@ -1,25 +1,12 @@
-class Configuration{
-    public string ApiHash{get=>apiHash;set{apiHash=value;}}
-    public string ApiId{get=>apiId;set{apiId=value;}}
-    private string id;
-    private string apiHash;
-    private string apiId;
-    private string phoneNumber;
-    private string session;
-    public Configuration()
-    {
-
+namespace TM
+{
+    class Configuration{
+        public string ApiHash{get=>apiHash;set{apiHash=value;}}
+        public string ApiId{get=>apiId;set{apiId=value;}}
+        private string id;
+        private string apiHash;
+        private string apiId;
+        private string phoneNumber;
+        private string session;
     }
-
-    public static Configuration GetConfigurationFromFile(string filename)
-    {
-        return new Configuration();
-    }
-
-    public static bool IsFileAvailable(string filename)
-    {
-        return true;
-    }
-
-    
 }
